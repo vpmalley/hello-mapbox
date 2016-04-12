213636cf067422006d7a273342291a4a8206126f
vince <vpmalley@gmail.com>
2016-04-13 00:01:58 +0200

    added marker

--- app/src/main/java/fr/vpm/hellomap/MapActivity.java
+++ app/src/main/java/fr/vpm/hellomap/MapActivity.java
@@ -3,7 +3,11 @@ package fr.vpm.hellomap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.mapbox.mapboxsdk.annotations.MarkerOptions;
+import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 
 public class MapActivity extends AppCompatActivity {
 
@@ -15,6 +19,17 @@ public class MapActivity extends AppCompatActivity {
     setContentView(R.layout.activity_map);
     findViews();
     mapview.onCreate(savedInstanceState);
+    mapview.getMapAsync(new OnMapReadyCallback() {
+      @Override
+      public void onMapReady(MapboxMap mapboxMap) {
+
+        MarkerOptions markerOptions = new MarkerOptions()
+            .position(new LatLng(48.19866,-69.86450))
+            .title("cool views here")
+            .getThis();
+        mapboxMap.addMarker(markerOptions);
+      }
+    });
   }
 
   private void findViews() {
